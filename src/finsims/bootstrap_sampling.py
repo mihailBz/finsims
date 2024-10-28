f980278765ad49bd0fc2e559f509b4bc35238546
Mykhailo Briazkalo <mbriazka@uwaterloo.ca>
2024-10-28 12:34:34 -0400

    update bootstrap samping

--- src/finsims/bootstrap_sampling.py
+++ src/finsims/bootstrap_sampling.py
@@ -1,25 +1,33 @@
 import numpy as np
 
 
-def stationary_block_bootstrap(historical_data, number_required=100, exp_block_size=20):
+def stationary_block_bootstrap(
+    historical_data, number_required=200, exp_block_size=20, num_paths=1000
+):
     N = len(historical_data)
-    bootstrap_samples = []
+    all_bootstrap_samples = []
 
-    while len(bootstrap_samples) < number_required:
-        # Choose a random starting index between 0 and N-1
-        index = np.random.randint(0, N)
+    for _ in range(num_paths):
+        bootstrap_samples = []
 
-        # Sample block size from a geometric distribution
-        blocksize = np.random.geometric(1 / exp_block_size)
+        while len(bootstrap_samples) < number_required:
+            # Choose a random starting index between 0 and N-1
+            index = np.random.randint(0, N)
 
-        # Generate the block of samples
-        for i in range(blocksize):
-            # Wrap around if the index exceeds the data length
-            sample_index = (index + i) % N
-            bootstrap_samples.append(historical_data[sample_index])
+            # Sample block size from a geometric distribution
+            blocksize = np.random.geometric(1 / exp_block_size)
 
-            # Stop if we have enough samples
-            if len(bootstrap_samples) == number_required:
-                return bootstrap_samples
+            # Generate the block of samples
+            for i in range(blocksize):
+                # Wrap around if the index exceeds the data length
+                sample_index = (index + i) % N
+                bootstrap_samples.append(historical_data[sample_index])
 
-    return bootstrap_samples
+                # Stop if we have enough samples
+                if len(bootstrap_samples) == number_required:
+                    break
+
+        # Append the generated path to the list of all paths
+        all_bootstrap_samples.append(bootstrap_samples[:number_required])
+
+    return all_bootstrap_samples
