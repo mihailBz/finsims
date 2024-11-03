a0c247123bd4d3eeb5086d442b616d2e5d3f2b2f
Mykhailo Briazkalo <mbriazka@uwaterloo.ca>
2024-11-03 17:43:21 -0500

    add zscore normalization for real dataset

--- src/finsims/create_real_dataset.py
+++ src/finsims/create_real_dataset.py
@@ -1,12 +1,12 @@
 import argparse
 import os
-
+import json
 import yfinance as yf
 from jsonl_utils import save_dataset
 from transformations import log_return
 
 
-def main(format_, data_dir, n, symbols):
+def main(format_, data_dir, n, symbols, normalization=None):
     for symbol in symbols:
         data = yf.download(symbol, period="max")
         data = data["Adj Close"]
@@ -14,6 +14,17 @@ def main(format_, data_dir, n, symbols):
         max_length = log_returns.shape[0] - log_returns.shape[0] % n
         log_returns = log_returns[:max_length].reshape(-1, n)
         os.makedirs(data_dir, exist_ok=True)
+        if normalization == 'zscore':
+            mean = log_returns.mean(axis=1)
+            std = log_returns.std(axis=1)
+            log_returns = (log_returns - mean[:, None]) / std[:, None]
+            zscore_params = {
+                "mean": mean.mean(),
+                "std": std.mean(),
+            }
+            zscore_params_f = f"{data_dir}/zscore-params.json"
+            with open(zscore_params_f, "w") as f:
+                json.dump(zscore_params, f, indent=4)
         save_dataset(f"{data_dir}/{symbol}_log_return", log_returns.T, format_)
 
 
@@ -52,6 +63,13 @@ if __name__ == "__main__":
         help="The symbols to download.",
     )
 
+    parser.add_argument(
+        "--normalization",
+        type=str,
+        default=None,
+        help="The normalization to apply to the dataset.",
+    )
+
     args = parser.parse_args()
 
-    main(args.format, args.data_dir, args.n, args.symbols)
+    main(args.format, args.data_dir, args.n, args.symbols, args.normalization)
